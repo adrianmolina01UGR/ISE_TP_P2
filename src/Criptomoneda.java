3947ecd1ca3c8828bf7488768a080d304928f9e1
adrianmolina01UGR <adrianmolina01@correo.ugr.es>
2023-10-17 16:30:11 +0200

    Primera versión del archivo Criptomoneda.java

---
+++ src/Criptomoneda.java
@@ -0,0 +1,9 @@
+public class Criptomoneda {
+	private String nombre;
+	private double valorDeToken;
+
+	public Criptomoneda(String nombre, double valorDeToken) {
+		this.nombre = nombre;
+		this.valorDeToken = valorDeToken;
+	}
+}
